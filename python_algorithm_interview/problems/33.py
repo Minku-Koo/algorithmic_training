19a759ad97e790ea13ed4f44b9e1412994f532d3
구민구 <koomk97@naver.com>
2021-04-14 05:03:11 +0900

    Create 33.py

---
+++ python_algorithm_interview/problems/33.py
@@ -0,0 +1,34 @@
+# 31.04.14
+# p.338
+# Problem Title : 전화 번호 문자 조합
+
+def solution(number):
+    phone = {
+        2 : "abc",
+        3 : "def",
+        4 : "ghi",
+        5 : "jkl",
+        6 : "mno",
+        7 : "pqrs",
+        8 : "tuv",
+        9 : "wxyz"
+    }
+
+    def dfs(path, index):
+        if len(path) == len(number):
+            result.append(path)
+            return
+        
+        for i in range(index, len(number)):
+            for d in phone[ int(number[i]) ]:
+                dfs(path+d, i+1)
+
+    result = []
+    dfs("", 0)
+
+    return result
+
+if __name__ == "__main__":
+    number = "256"
+    result = solution(number)
+    print(result)
