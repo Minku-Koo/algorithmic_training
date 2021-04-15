2016947df5ec333deda9bb131d0b310ea96be1b6
구민구 <koomk97@naver.com>
2021-04-15 20:55:48 +0900

    update search algorithm

--- python_algorithm_interview/study/bfs.py
+++ python_algorithm_interview/study/bfs.py
@@ -11,17 +11,18 @@ graph = {
 # BFS는 재귀 함수 사용 불가
 
 # Queue를 이용한 BFS
+# Input : 루트 노드 값
 def iterative_bfs(root):
-    discovered = [root]
-    queue = [root]
-    while queue:
-        x = queue.pop(0)
+    discovered = [root] # 탐색한 노드에 루트 노드 추가
+    queue = [root] # 큐에 루트 노드 추가
+    while queue: # 큐가 빌 때 까지 반복
+        x = queue.pop(0) # 큐에서 꺼내옴
         
-        for node in graph[x]:
-            if node not in discovered:
-                discovered.append(node)
-                queue.append(node)
+        for node in graph[x]: # 해당 노드의 모든 하위 노드
+            if node not in discovered: # 탐색한 적이 없으면
+                discovered.append(node) # 탐색한 노드에 추가
+                queue.append(node) # 큐에 추가
 
-    return discovered
+    return discovered #탐색 결과
 
 print( iterative_bfs(1) )
--- python_algorithm_interview/study/dfs.py
+++ python_algorithm_interview/study/dfs.py
@@ -9,24 +9,26 @@ graph = {
 }
 
 # 재귀 함수 이용한 DFS
+# Input : 루트 노드 값
 def recursion_dfs(node, discovered=[]):
-    discovered.append(node)
-    for x in graph[node]:
-        if x not in discovered:
-            discovered = recursion_dfs(x, discovered)
-    return discovered
+    discovered.append(node) # 탐색한 노드에 추가
+    for x in graph[node]: # 해당 노드 하위의 모든 노드
+        if x not in discovered: # 탐색한 적 없으면
+            discovered = recursion_dfs(x, discovered) # 재귀 함수 실행
+    return discovered #최종 탐색 결과
 
 # Stack 이용한 DFS
+# Input : 루트 노드 값
 def stack_dfs(node):
     discovered = []
-    stack = [node]
-    while stack:
-        v = stack.pop()
-        if v not in discovered:
-            discovered.append(v)
-            for w in graph[v]:
-                stack.append(w)
-    return discovered
+    stack = [node] #최초 루트 노드
+    while stack: # 스택이 빌 때 까지
+        v = stack.pop() # 스택에서 꺼내옴
+        if v not in discovered: # 탐색한 적이 없으면
+            discovered.append(v) # 탐색한 노드에 추가
+            for w in graph[v]: # 해당 노드의 모든 하위 노드
+                stack.append(w) # 스택에 추가
+    return discovered #최종 탐색 결과
 
 print("="*5,"Recursion","="*5)
 print( recursion_dfs(1) )
