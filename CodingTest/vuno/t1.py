209b15335b56a87e13be8c69d286a9fd4cb4bb12
Minku Koo <koomk97@naver.com>
2023-08-21 23:03:24 +0900

    Restart algorithm study

--- CodingTest/vuno/t1.py
+++
@@ -1,8 +0,0 @@
-#
-
-n = int(input())
-layers = [ int(value) for value in input().split() ]
-
-answer = sum([j*layers[index-1] if index!=0 else 0 for index, j in enumerate(layers)  ])
-
-print(answer)
